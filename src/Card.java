02d3c0b187a7d8bd7d2d1320a935da35fbce425f
Damion <na>
2023-04-14 17:30:43 -0400

    Initial Commit

---
+++ src/Card.java
@@ -0,0 +1,99 @@
+import java.awt.*;
+public class Card {
+
+    //fields-------------
+
+    public static final char CLUBS = 'c';
+
+    public static final char DIAMONDS = 'd';
+
+    public static final char HEARTS = 'h';
+
+    public static final char SPADES = 's';
+
+    public static final int LOWEST_VALUE = 2;
+
+    public static final int HIGHEST_VALUE = 14;
+
+    char suit = "";
+    int value = 0;
+    Color color = 0;
+    boolean hasBeenFound = false;
+
+
+
+
+    
+    //class constructor----------------------
+
+    public Card(char suit, int value){
+        this.suit = suit;
+        this.value = value;
+
+        if(!(suit == 'c' || suit == 'h' || suit == 's' || suit == 'd')){
+            throw IllegalArgumentException("Invalid Suit");
+        } else if (value > 14 || value < 2) {
+            throw IllegalArgumentException("Invalid Value");
+        }
+
+    } 
+
+   
+   //methods--------------------------
+   
+    public char getSuit(){
+        return this.suit;
+        
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public Color getColor(){
+        Color color = color.YELLOW;
+
+        if(this.suit == 'c' || this.suit == 's'){
+            color = Color.BLACK;
+        }
+        if(this.suit == 'h' || this.suit == 'd'){
+            color = Color.RED;
+        }
+
+        return color;
+    }
+
+    public boolean hasBeenFound(){
+        
+        return  this.hasBeenFound;
+    }
+
+    public void setHasBeenFound(boolean hasBeenFound){
+        
+    }
+
+    public boolean hasSameValue(Card other){
+        if (this.value.equals(other.value)){
+            this.hasSameValue = true;
+        } else {
+            this.hasSameValue = false;
+        }
+
+        return this.hasSameValue;
+    }
+
+    public boolean hasSameValueAndColor(Card other){
+        if (this.color.equals(other.color) && this.hasSameValue){
+            hasSameValueAndColor
+        }
+    }
+
+    public boolean equals(Object o) {
+
+    }
+
+    public String toString(){
+
+    }
+
+}
---
+++ src/MatchGame.java
@@ -0,0 +1,9 @@
+public class MatchGame {
+
+    public static final int ROWS = 4; 
+
+    public static final int COLS = 13;
+
+    
+    
+}
