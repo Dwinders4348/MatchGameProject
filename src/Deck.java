d1baed050672c17c3ac03ae893e99872079042f2
Damion <na>
2023-04-15 20:19:44 -0400

    upated Deck.java, almost complete

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Deck {
     
     //Constants-----------------------------
@@ -14,21 +16,30 @@ public class Deck {
 
 
     //Constructor----------------------------------------
+   
     public Deck(){
         
          //Set cards array with cards
-        for (int i = 0; i < 13; i++) {
-            cards[i] = new Card('c', i+2);
+
+        int index = 0; 
+        
+        for(int i = 2; i < 14; i++) {
+            cards[index] = new Card('c', i);
+            index++;
         }
-        for (int j = 13; j > 12 && j < 24; j++) {
-            cards[j] = new Card('d', j + 2);
+        for(int i = 2; i < 14; i++) {
+            cards[index] = new Card('d', i);
+            index++;
         }
-        for (int k = 24; k < 36; k++){
-            cards[k] = new Card('h', k + 2);
+        for(int i = 2; i < 14; i++) {
+            cards[index] = new Card('h', i);
+            index++;
         }
-        for (int l = 36; l < 52; l++ ){
-            cards[l] = new Card('s', l+2);
+        for(int i = 2; i < 14; i++) {
+            cards[index] = new Card('s', i);
+            index++;
         }
+
 
         next = 0;
 
@@ -48,19 +59,59 @@ public class Deck {
         // for (i = n - 1; down to 1 do
         // swap a[i] with a random element among a[0],...,a[i]
         // (so if the chosen element is a[i], it stays put)
+
+        for (int i = CARDS_IN_DECK - 1; i >= 0; i--){
+            Random rand = new Random();
+            int num = rand.nextInt(51); 
+            Card temp = cards[i];
+            cards[i] = cards[num];
+            cards[num] = temp;
+
+        }
+
   
     }
 
-    public Card nextCard(){
-        return cards[next];
+    public Card nextCard() throws IllegalStateException {
+        if(next >= CARDS_IN_DECK) {
+            throw new IllegalStateException("No more cards");
+        }
+        next++;
+        return cards[next - 1];
     }
     
     public boolean equals(Object o){
+        
+        Deck d;
+
+        if((o instanceof Deck)){
+            d = (Deck)o;
+        } else {
+            return false;
+        }
+
+
+        if((!(Arrays.equals(d.cards, this.cards) && (this.next == d.next)))) {
+            return false;
+        }
+       
+
         return true;
     }
 
     public String toString(){
-        return "";
+        String deck = ""; 
+        
+        
+        for (int i = 0; i < 52; i++){
+        //   String add = cards[i].toString();
+          deck += "Card " + i + " " + cards[i].toString() + "\n";
+
+        }
+
+        
+        
+        return deck;
     }
 
 
